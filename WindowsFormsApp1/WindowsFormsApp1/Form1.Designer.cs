f3b2a4a4f7d9a7ee6a4676d7d947b199cfdbb835
olya lancz <olya.lancz.02@mail.ru>
2022-02-12 16:09:46 +0300

    update events

--- WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -65,6 +65,7 @@ namespace WindowsFormsApp1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.WindowFrame;
             this.ClientSize = new System.Drawing.Size(421, 102);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
